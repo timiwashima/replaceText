9cf3870169a9d5a6f743fd36449e1283ba33f55e
timiwashima <52045865+timiwashima@users.noreply.github.com>
2023-09-08 09:09:54 -0700

    replace.py

---
+++ replace.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 11:40:56 2023
+
+@author: tiwashima
+"""
+
+originalData='Hello, Tim'
+replaceData=originalData.replace('Tim', 'World')
+#New line can be specified with \n
+print(replaceData)
